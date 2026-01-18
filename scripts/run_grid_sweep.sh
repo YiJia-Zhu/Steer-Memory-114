c5a51cedc8b3026106bc8214d0478d250ad4931f
YiJia-Zhu <2175912978@qq.com>
2026-01-18 17:11:03 +0800

    math parser

--- scripts/run_grid_sweep.sh
+++ scripts/run_grid_sweep.sh
@@ -6,8 +6,8 @@ set -m
 # User-editable (TOP)
 # =========================
 # GPUs to use for parallel runs. One job will reserve tensor_parallel_size GPUs.
-# GPUS="${GPUS:-0,1,2,3,4,5,6,7}"
-GPUS="${GPUS:-0}"
+GPUS="${GPUS:-0,1,2,3,4,5,6,7}"
+# GPUS="${GPUS:-0}"
 
 # -------------------------
 # Models to sweep
@@ -16,8 +16,8 @@ GPUS="${GPUS:-0}"
 MODEL_SPECS=(
   "ds_r1_qwen_1p5b|huggingface_models/DeepSeek-R1-Distill-Qwen-1.5B|1|512"
   "qwen2p5_3b|huggingface_models/Qwen2.5-3B-Instruct|1|512"
-  # "ds_r1_qwen_7b|huggingface_models/DeepSeek-R1-Distill-Qwen-7B|1|256"
-  # "qwen2p5_7b|huggingface_models/Qwen2.5-7B-Instruct|1|256"
+  "ds_r1_qwen_7b|huggingface_models/DeepSeek-R1-Distill-Qwen-7B|1|256"
+  "qwen2p5_7b|huggingface_models/Qwen2.5-7B-Instruct|1|256"
 )
 
 # -------------------------
@@ -31,12 +31,12 @@ MODEL_SPECS=(
 # - Use "__KEEP__" to keep BASE_CFG values for that field.
 # -------------------------
 DATASET_SPECS=(
-  # "math500|math_0shot|test|test|100|400|16384|16384"
+  "math500|math_0shot|test|test|100|400|16384|16384"
   # "aime_2024|math_0shot|train|train|10|20|16384|16384"
   # "amc23|math_0shot|test|test|10|30|16384|16384"
   # "aime25|math_0shot|test|test|10|20|16384|16384"
-  "arc-c|arc_0shot|train|validation|100|null|1024|4096" # 1.12k 299
-  "openbookqa|arc_0shot|train|validation|100|null|1024|4096" # 4k 500 
+  # "arc-c|arc_0shot|train|validation|100|null|1024|4096" # 1.12k 299
+  # "openbookqa|arc_0shot|train|validation|100|null|1024|4096" # 4k 500 
   # "gsm8k|gsm8k_0shot|train|test|100|null|2048|4096"
   # "commonsense_qa|arc_0shot|train|validation|100|null|1024|4096" # 9k 1k
 )
@@ -61,7 +61,7 @@ LAYER_LIST=(0.2 0.4 0.6 0.8)
 # Reward 函数中回答长度权重
 ETA0_LIST=(0.01)
 # 注入强度
-KSCALE_LIST=(0.25 0.5 0.75 1.0)
+KSCALE_LIST=(0.05 0.25 0.1 0.2 0.4 0.5 0.75 1.0)
 
 # LAYER_LIST=(0.8)
 # # Reward 函数中回答长度权重
